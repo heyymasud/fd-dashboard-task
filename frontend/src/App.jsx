1aa510d33b5cfc6b368bda1bf17ebb0ecf7f709b
heyymasud <heyymasud@gmail.com>
2024-07-29 06:36:39 +0700

    feat: auth login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layouts/Home'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layouts/Home";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProtectedRoute from "./components/Layouts/Home/ProtectedRoute";
 
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
