627312373eb8623f1ecf985021956c682adf3196
heyymasud <heyymasud@gmail.com>
2024-07-29 02:35:48 +0700

    feat: dashboard page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import ProjectStatisctics from "../components/Layouts/Home/ProjectStatisctics";
+import RevenueCard from "../components/Layouts/Home/RevenueCard";
+
+
+const Home = () => {
+  return (
+    <div className="p-5">
+      <div className="flex flex-col gap-3">
+        <RevenueCard />
+        <ProjectStatisctics />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
