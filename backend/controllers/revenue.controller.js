804255931003d38a73e0b2fd3c5bd7b5abc3ef28
heyymasud <heyymasud@gmail.com>
2024-07-29 01:58:33 +0700

    finish backend setup

--- backend/controllers/revenue.controller.js
+++ backend/controllers/revenue.controller.js
@@ -1,7 +1,19 @@
+const { Revenues } = require("../models");
+const sequelize = require("sequelize");
+
 const getAllRevenues = async (req, res) => {
-    return res.status(200).json({ message: 'getAllRevenues' });
-}
+  const revenues = await Revenues.findAll({
+    attributes: [ // menentukan kolom yang ingin diambil ok
+      [sequelize.fn("SUM", sequelize.col("total")), "total"],
+      [sequelize.fn("YEAR", sequelize.col("createdAt")), "year"], 
+      [sequelize.fn("MONTH", sequelize.col("createdAt")), "month"],
+    ], // mengelompokkan data berdasarkan tahun dan bulan ok
+    group: [[sequelize.fn("YEAR", sequelize.col("createdAt"))],[sequelize.fn("MONTH", sequelize.col("createdAt"))]],
+  });
+
+  return res.status(200).json(revenues);
+};
 
 module.exports = {
-    getAllRevenues
-}
+  getAllRevenues,
+};
