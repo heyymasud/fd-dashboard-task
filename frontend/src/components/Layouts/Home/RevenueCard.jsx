337bf2b94d9530f1e96f98329e9f171572dba467
heyymasud <heyymasud@gmail.com>
2024-07-29 08:08:24 +0700

    fe add

--- frontend/src/components/Layouts/Home/RevenueCard.jsx
+++ frontend/src/components/Layouts/Home/RevenueCard.jsx
@@ -5,8 +5,8 @@ const RevenueCard = ({ revenues }) => {
     <div className="px-5 bg-white rounded-xl">
       <h1 className="text-2xl font-bold text-gray-700">Revenue</h1>
       <div className="p-2 grid sm:grid-cols-3 xl:grid-cols-6 gap-2 overflow-auto xl:h-auto h-20">
-        {revenues.map((revenue) => (
-          <div key={revenue.id} className="space-y-1 text-gray-500">
+        {revenues.map((revenue, index) => (
+          <div key={index} className="space-y-1 text-gray-500">
             <p className="flex items-center space-x-2">
               <GiProfit /> <span>{revenue.month}</span>
             </p>
